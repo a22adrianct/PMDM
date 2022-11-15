f70d7e1c82394c21f47bd9e64041cb84ddb56a8d
Adrián Cervera <a22adrianct@iessanclemente.net>
2022-11-15 12:08:20 +0100

    Clase 15/11/2022

--- I3BA22AdrianCT/app/src/main/java/com/example/i3b_a22adrianct/IntroducirDatos.java
+++ I3BA22AdrianCT/app/src/main/java/com/example/i3b_a22adrianct/IntroducirDatos.java
@@ -5,26 +5,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class IntroducirDatos extends AppCompatActivity {
 
+    private EditText etCadea, etTelefono;
+    private Button close;
+    private Bundle bundle = new Bundle();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_introducir_datos);
-    }
 
-    EditText etCadea = findViewById(R.id.cadea);
-    EditText etTelefono = findViewById(R.id.telefono);
+        etCadea = findViewById(R.id.cadea);
+        etTelefono = findViewById(R.id.telefono);
+        close = findViewById(R.id.pechar);
 
-    public void onPecharClick(View v) {
-        Intent datosVolta = new Intent();
-        datosVolta.putExtra("TEXTO", etCadea.getText().toString());
-        datosVolta.putExtra("TELEFONO", etTelefono.getText().toString());
-        setResult(RESULT_OK, datosVolta);
-        finish();
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+
+                bundle.putString("TEXTO", etCadea.getText().toString());
+                bundle.putString("TELEFONO", etTelefono.getText().toString());
+
+                i.putExtras(bundle);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 
     public void finish(){
--- I3BA22AdrianCT/app/src/main/java/com/example/i3b_a22adrianct/MainActivity.java
+++ I3BA22AdrianCT/app/src/main/java/com/example/i3b_a22adrianct/MainActivity.java
@@ -1,47 +1,102 @@
 package com.example.i3b_a22adrianct;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
-import androidx.fragment.app.FragmentManager;
 
-import android.app.Activity;
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.SearchManager;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.net.InetSocketAddress;
+import java.nio.BufferUnderflowException;
+
 public class MainActivity extends FragmentActivity {
 
-    private String texto = "casa";
+    private String texto;
     private String numTelf;
-    private static final int CODIGO = 8;
+    private Button nextActivity, dataButton;
+    private Bundle bundle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void onBtnClick(View view){
+        nextActivity = (Button) findViewById(R.id.telephoneButton);
+        dataButton = (Button) findViewById(R.id.dataButton);
+
+        bundle = getIntent().getExtras();
+
         Intent intent = new Intent(this, IntroducirDatos.class);
-        startActivity(intent);
-    }
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == CODIGO) {
-            if (resultCode == RESULT_OK) {
-                if (data.hasExtra("TEXTO")) {
-                    texto = data.getExtras().getString("TEXTO");
-                }
-                 if (data.hasExtra("TELEFONO")) {
-                     numTelf = data.getExtras().getString("TELEFONO");
-                } else {
-                     Toast.makeText(this, "Non intruduciches un número de teléfono", Toast.LENGTH_SHORT).show();
-                 }
+        nextActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent);
             }
-        }
+        });
+
+        nextActivity.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Escolle unha opción");
+                builder.setCancelable(false);
+                builder.setPositiveButton("Chamar", (DialogInterface.OnClickListener) (dialog, wich) -> {
+                    if (!bundle.getString("TELEFONO").isEmpty()) {
+                        Intent call = new Intent(Intent.ACTION_CALL);
+                        call.setData(Uri.parse(bundle.getString("TELEFONO")));
+                        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                            MainActivity.this.requestPermissions(new String[]
+                                            {Manifest.permission.CALL_PHONE},
+                                    1);
+                        }
+                        startActivity(call);
+                    }
+                    else {
+                        Toast.makeText(getApplicationContext(), "O campo teléfono está baleiro", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                builder.setNegativeButton("Buscar", (DialogInterface.OnClickListener) (dialog, wich) -> {
+                    Intent search = new Intent(Intent.ACTION_WEB_SEARCH);
+                    if (bundle.getString("TEXTO").isEmpty()){
+                        search.putExtra(SearchManager.QUERY, texto);
+                    } else {
+                        search.putExtra(SearchManager.QUERY, bundle.getString("TEXTO"));
+                    }
+                    startActivity(search);
+                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+                return false;
+            }
+        });
+        dataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (!bundle.getString("TEXTO").isEmpty()){
+                    texto = bundle.getString("TEXTO");
+                } else {
+                    texto = "casa";
+                }
+                if (!bundle.getString("TELEFONO").isEmpty()){
+                    numTelf = bundle.getString("TELEFONO");
+                } else {
+                    numTelf = "";
+                }
+                Toast.makeText(getApplicationContext(), texto + "\n" + numTelf, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
