3df26a40ef1e82af8ee899098005e9bcc7f11a9a
Ubuntu casa <a22adrianct@iessanclemente.net>
2023-02-13 23:26:41 +0100

    Casa 13/02/2023

---
+++ A1AA22AdrianCT/app/src/main/java/com/example/a1_a_a22adrianct/ActivityDatos.java
@@ -0,0 +1,15 @@
+package com.example.a1_a_a22adrianct;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityDatos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datos);
+    }
+}
